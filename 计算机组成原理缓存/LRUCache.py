445566d1733c7ab8e4a07625a9db001e2d620f0f
zzdxlizhihan <2267234389@qq.com>
2020-02-24 15:55:43 +0800

    增加两个算法

---
+++ 计算机组成原理缓存/LRUCache.py
@@ -0,0 +1,59 @@
+# -*- coding=utf-8 -*-
+
+from doublelinked import DoubleLinkedList, Node
+
+class LRUCache(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.map = {}
+        self.list = DoubleLinkedList(self.capacity)
+
+    def get(self, key):
+        if self.capacity == 0:
+            return
+        if key in self.map:
+            node = self.map.get(key)
+            self.list.remove(node)
+            self.list.append_front(node)
+            return node.value
+        else:
+            return -1
+
+    def put(self, key, value):
+        if key in self.map:
+            node = self.map.get(key)
+            self.list.remove(node)
+            node.value = value
+            self.list.append_front(node)
+            self.map[key] = node
+        else:
+            node = Node(key, value)
+            # 缓存已经满了
+            if self.list.size >= self.list.capacity:
+                old_node = self.list.remove()
+                self.map.pop(old_node.key)
+                self.list.append_front(node)
+                self.map[key] = node
+            #缓存还没满
+            else:
+                self.list.append_front(node)
+                self.map[key] = node
+    
+    def print(self):
+        return self.list.print()
+
+    
+if __name__ == '__main__':
+    cache = LRUCache(2)
+    cache.put(2, 2)
+    cache.print()
+    cache.put(1, 1)
+    cache.print()
+    cache.put(3 ,3)
+    cache.print()
+    print(cache.get(1))
+    cache.print()
+    print(cache.get(2))
+    cache.print()
+    print(cache.get(3))
+    cache.print()
